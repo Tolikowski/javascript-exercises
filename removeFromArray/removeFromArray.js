42505fff92713a3f2bbc6356697f2aafa26f5d00
Anatoli Miske <hello@anatolimiske.de>
2021-07-01 20:49:21 +0200

    solve RemoveFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+
+let newArray = arr;
+let target = args;
+
+for (let i = 0; i < args.length; i++){
+    if (newArray.includes(args[i])){
+        newArray.splice(newArray.indexOf(args[i]),1)
+    }
+}
+
+return newArray;
 
 };
 
