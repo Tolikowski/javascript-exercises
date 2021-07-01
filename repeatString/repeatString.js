c857aa065c6edda6fef352dd072786f3d901f781
Anatoli Miske <hello@anatolimiske.de>
2021-07-01 16:02:39 +0200

    upload repo with 4 solved challenges

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let string = "";
+    if (num < 0) {
+        string = "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            string += str;
+        };
+    }
 
+    return string;
 };
 
 module.exports = repeatString;
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let numbers = [a, b];
+
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (!(typeof a === "number") || !(typeof b === "number")) {
+        return "ERROR";
+    } else {
+        numbers.sort();
+
+        for (i = numbers[0]; i <= numbers[1]; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
 
 };
 
